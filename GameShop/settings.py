77a6f4c45a2ddad7cdb45539b669cc92abbdffa1
MaxNet <White-fox2017@mail.ru>
2021-04-29 18:34:21 +0400

    add tags

--- GameShop/settings.py
+++ GameShop/settings.py
@@ -84,16 +84,25 @@ WSGI_APPLICATION = 'GameShop.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
- 'default': {
-     'ENGINE': 'django.db.backends.postgresql',
-     'NAME': 'GameShop',
-     'USER': 'postgres',
-     'PASSWORD': '1234',
-     'HOST': 'localhost',
-     'PORT': '',
- }
+  'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'sqlite3',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
 }
-
+# DATABASES = {
+#  'default': {
+#      'ENGINE': 'django.db.backends.postgresql',
+#      'NAME': 'GameShop',
+#      'USER': 'postgres',
+#      'PASSWORD': '1234',
+#      'HOST': 'localhost',
+#      'PORT': '',
+#  }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
